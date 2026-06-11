217ab9b4216946e0ab13b3ca7151793cabb0c751
kaneko <syogo13578rgd@gamil.com>
2026-06-11 17:05:00 +0900

    6/11進捗ここまで

--- app/models.py
+++ app/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    class Meta:
+        db_table = "account_user"
+    user_id = models.CharField(verbose_name="会員ID", max_length=128, primary_key=True)
+    password = models.CharField(verbose_name="パスワード", max_length=256)
+    name = models.CharField(verbose_name="名前", max_length=128)
+    address = models.CharField(verbose_name="住所", max_length=256)
+
+
+class Category(models.Model):
+    class Meta:
+        db_table = "shopping_category"
+    category_id = models.IntegerField(verbose_name="カテゴリID", primary_key=True)
+    name = models.CharField(verbose_name="カテゴリ名", max_length=256)
+
+
+class Item(models.Model):
+    class Meta:
+        db_table = "shopping_item"
+    item_id = models.IntegerField(verbose_name="商品ID", primary_key=True)
+    name = models.CharField(verbose_name="商品名", max_length=128)
+    manufacturer = models.CharField(verbose_name="メーカー名", max_length=32)
+    color = models.CharField(verbose_name="商品の色", max_length=16)
+    price = models.IntegerField(verbose_name="価格")
+    stock = models.IntegerField(verbose_name="在庫数")
+    recommended = models.BooleanField(verbose_name="おススメ", default=False)
+    category = models.ForeignKey(Category, verbose_name="カテゴリ", on_delete=models.CASCADE)
+
+
+class Itemincart(models.Model):
+    class Meta:
+        db_table = "shopping_itemsincart"
+    amount = models.IntegerField(verbose_name="数量")
+    booked_date = models.DateTimeField(verbose_name="登録日", auto_now_add=True)
+    item = models.ForeignKey(Item, verbose_name="商品", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name="会員", on_delete=models.CASCADE)
+
+
+class Purchase(models.Model):
+    class Meta:
+        db_table = "shopping_purchase"
+    purchase_id = models.IntegerField(verbose_name="注文ID")
+    destination = models.CharField(verbose_name="配送先", max_length=256)
+    booked_date = models.DateTimeField(verbose_name="注文日", auto_now_add=True)
+    cancel = models.BooleanField(verbose_name="キャンセル", default=False)
+    user = models.ForeignKey(User, verbose_name="注文者", on_delete=models.CASCADE)
+
+
+class Purchasedetail(models.Model):
+    class Meta:
+        db_table = "shopping_purchasedetail"
+    purchase_detail_id = models.IntegerField(verbose_name="注文詳細ID", primary_key=True)
+    amount = models.IntegerField(verbose_name="注文数")
+    item = models.ForeignKey(Item, verbose_name="商品", on_delete=models.CASCADE)
+    purchase = models.ForeignKey(Purchase, verbose_name="注文", on_delete=models.CASCADE)
+
+
+class Admin(models.Model):
+    class Meta:
+        db_table = "administrator_admin"
+    admin_id = models.CharField(verbose_name="管理者ID", max_length=128, db_index=True)
+    password = models.CharField(verbose_name="パスワード", max_length=256)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import CreateView
+from app.models import User, Category
+from django.urls import reverse_lazy
+from .forms import UserCreateForm, LoginForm
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your views here.
+def index(request):
+    categories = Category.objects.all()
+    login_user = None
+    if request.session.get("is_login"):
+        login_user = User.objects.filter(user_id=request.session['login_user_id']).first()
+    return render(request, "app/base.html", {"categories": categories,'login_user': login_user})
+
+
+class UserCreate(CreateView):
+    model = User
+    form_class = UserCreateForm
+    template_name = "app/signup.html"
+    success_url = reverse_lazy("app:login")
+
+    def form_valid(self, form):
+        # パスワードをハッシュ化して保存
+        form.instance.password = make_password(form.cleaned_data["password"])
+        return super().form_valid(form)
+
+
+def login_view(request):
+    form = LoginForm()
+    print("aaa")
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            password = form.cleaned_data["password"]
+
+            # ① DBからユーザーを探す
+            user = User.objects.filter(name=name).first()
+
+            # ② パスワードが一致するか確認
+            if user and check_password(password, user.password):
+                # ③ セッションに保存（＝ログイン状態にする）
+                request.session["is_login"] = True
+                request.session["login_user_id"] = user.user_id
+                request.session["login_user_name"] = user.name
+                return redirect("app:index")
+                # return render(request, "app/login.html", {"form": form})
+
+            # ④ 失敗
+            form.add_error(None, "ユーザー名またはパスワードが違います")
+
+    return render(request, "app/login.html", {"form": form})
+
+
+def logout_view(request):
+    request.session.flush()  # セッション全削除
+    return redirect("app:login")
+
+
+def user_list_view(request):
+    # ログインチェック
+    if not request.session.get("is_login"):
+        return redirect("app:login")
+
+    users = User.objects.all()
+    return render(request, "app/snsuser_list.html", {
+        "users": users,
+        "login_user_name": request.session.get("login_user_name"),
+    })
+
+def search_view(request):
+    return redirect("app:")
